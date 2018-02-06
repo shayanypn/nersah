1f7da68f0d33a91530d6ddd53156336f8121d4da
shayan <shayan@faragirrfid.ir>
2018-02-06 14:49:26 +0330

    add node server for ajax request and testing

---
+++ server/server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express()
+const DELAY = 1000;
+
+
+app.get('/', (req, res) => {
+	res.send({
+		status: 200,
+		msg: 'Dev server is running!'
+	})
+});
+app.get('/api/', (req, res) => {
+	res.send({
+		status: 200,
+		version: '0.0.1',
+		msg: 'Dev server is running!'
+	})
+});
+app.get('/api/get', (req, res) => {
+
+	setTimeout(function(){
+		res.send({
+			status: 200,
+			msg: 'Dev server is running!'
+		})
+	},DELAY)
+})
+app.get('/api/post', (req, res) => {
+
+	setTimeout(function(){
+		res.send({
+			status: 200,
+			msg: 'Dev server is running!'
+		})
+	},DELAY)
+})
+
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
